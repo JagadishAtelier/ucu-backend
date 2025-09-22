6430b56bf83401d6d478a9675baadd822afbbeb3
JagadishRamakrishnan <jeevasumathi1357@gmail.com>
2025-09-22 17:16:01 +0530

    updated cart api

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ const app = express();
 require("dotenv").config();
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors({
   origin: "*",  // allows requests from any domain
   credentials: true, // optional: only needed if you use cookies or auth headers
